ed4cddaf108c7406ff8cd3ab34164a3ff27e024a
mrchlblsnyh <112853228+mrchlblsnyh@users.noreply.github.com>
2023-05-24 00:02:41 +0700

    HOME , CHART, TABLE
    Penambahan Home ,Chart,Table

---
+++ 1_🏠_Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Badan Pusat Statik"
+)
+
+st.title("Badan Pusat Statik")
---
+++ pages/3__📁_Table.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+st.set_page_config(
+    page_title="Badan Pusat Statik"
+)
+
+st.title("Table Page")
