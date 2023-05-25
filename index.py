f9df8aee2b77426e3a887347bf9dbe112a41367b
mrchlblsnyh <112853228+mrchlblsnyh@users.noreply.github.com>
2023-05-25 22:27:56 +0700

    DATA TABLE

--- index.py
+++ index.py
@@ -1,19 +1,33 @@
 import streamlit as st
-import pandas as pd
 from getAPI import GetAPI
 
-
 getAPI = GetAPI()
-getAPI.set_url = 'sp2020/5/1/3'
+getAPI.set_url = 'sp2020/1/1/3'
 
-getData = {}
+st.markdown("<h2 style='text-align: center;'> Jumlah Penduduk yang Pindah menurut Wilayah dan Jenis Kelamin</h2>", unsafe_allow_html=True)
+st.write("\n\n")
 
+table_html = "      <table>\n"
+table_html += "  <tr>\n"
+table_html += "    <th><center>Nama Provinsi</th>\n"
+table_html += "    <th>Laki - Laki</th>\n"
+table_html += "    <th>Perempuan</th>\n"
+table_html += "    <th><center>Total</center></th>\n"
+table_html += "  </tr>\n"
+
+sortData = {}
 for row in getAPI.getResponse("data"):
-    namaWilayah = row["nama_wilayah"]
-    if namaWilayah not in getData:
-        getData[namaWilayah] = []
-    getData[namaWilayah].append(row["nilai"])
+    sortedValue = row['nama_wilayah']
+    if sortedValue not in sortData:
+        sortData[sortedValue] = []
+    sortData[sortedValue].append(row['nilai'])
 
+for sortedValue, values in sortData.items():
+    table_html += "  <tr>\n"
+    table_html += f"    <td>{sortedValue}</td>\n"
+    for value in values:
+        table_html += f"      <td>{value}</td>\n"
+    table_html += "  </tr>\n"
 
-data_frame = pd.DataFrame(getData)
-st.table(data_frame)
+table_html += "</table>"
+st.markdown(table_html, unsafe_allow_html=True)
