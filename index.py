98cdb89ff503c84bd33aa7de673d243edc55876c
dikhaarianda <dikhaarianda@gmail.com>
2023-05-25 12:19:54 +0700

    update: show table

--- index.py
+++ index.py
@@ -6,14 +6,29 @@ from getAPI import GetAPI
 getAPI = GetAPI()
 getAPI.set_url = 'sp2020/5/1/3'
 
-getData = {}
+table_html = "<table>\n"
+table_html += "  <tr>\n"
+table_html += "    <th>Nama Wilayah</th>\n"
+table_html += "    <th>Nilai</th>\n"
+table_html += "  </tr>\n"
 
+sortData = {}
 for row in getAPI.getResponse("data"):
-    namaWilayah = row["nama_wilayah"]
-    if namaWilayah not in getData:
-        getData[namaWilayah] = []
-    getData[namaWilayah].append(row["nilai"])
+    sortedValue = row['nama_wilayah']
+    if sortedValue not in sortData:
+        sortData[sortedValue] = []
+    sortData[sortedValue].append(row['nilai'])
 
+for sortedValue, values in sortData.items():
+    table_html += "  <tr>\n"
+    table_html += f"    <td rowspan={len(values)}>{sortedValue}</td>\n"
+    table_html += f"    <td>{values[0]}</td>\n"
+    table_html += "  </tr>\n"
 
-data_frame = pd.DataFrame(getData)
-st.table(data_frame)
+    for value in values[1:]:
+        table_html += "  <tr>\n"
+        table_html += f"    <td>{value}</td>\n"
+        table_html += "  </tr>\n"
+
+table_html += "</table>"
+st.markdown(table_html, unsafe_allow_html=True)
